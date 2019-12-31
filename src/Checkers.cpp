6b67a4b98e983fdc8cc8976ea7d0a3d5056453fa
mtepfer <mtepfer1@gmail.com>
2019-12-30 19:21:41 -0500

    Added partial checking if a move is legal

--- src/Checkers.cpp
+++ src/Checkers.cpp
@@ -29,12 +29,55 @@ bool Checkers::move( std::pair<int, int> start, std::pair<int, int> end ) {
     if ( end.first < 0 || end.first >= 8 ) return false;
     if ( end.second < 0 || end.second >= 8 ) return false;
 
-    // make sure moving from non-empty cell to empty cell
-    if ( boardState()[ start.first ][ start.second ] == CellState::EMPTY ) return false;
-    if ( boardState()[ end.first ][ end.second ] != CellState::EMPTY ) return false;
+    // make sure end cell is on the right color square
+    if (( end.first + end.second ) % 2 == 0 ) return false;
 
-    // TODO check if move is legal
-    boardState_[ end.first ][ end.second ]     = boardState()[ start.first ][ start.second ];
-    boardState_[ start.first ][ start.second ] = CellState::EMPTY;
+    CellState& startCell   = boardState_[ start.first ][ start.second ];
+    CellState& endCell     = boardState_[ end.first ][ end.second ];
+    CellState* captureCell = nullptr;
+
+    // make sure moving from non-empty cell to empty cell
+    if ( startCell == CellState::EMPTY ) return false;
+    if ( endCell != CellState::EMPTY ) return false;
+
+    // make sure player isn't moving out of turn
+    if ( redTurn && startCell != CellState::RED && startCell != CellState::RED_KING ) return false;
+    if ( !redTurn && startCell != CellState::BLACK && startCell != CellState::BLACK_KING ) return false;
+
+    // make sure regular pieces aren't moving backwards
+    if ( startCell == CellState::RED && end.second > start.second ) return false;
+    if ( startCell == CellState::BLACK && end.second < start.second ) return false;
+
+    // make sure piece isn't moving sideways
+    if ( start.first == end.first || start.second == end.second ) return false;
+
+    // make sure piece isn't moving too far
+    if ( std::abs( start.first - end.first ) > 2 || std::abs( start.second - end.second ) > 2 ) return false;
+    if ( std::abs( start.first - end.first ) == 2 ) {
+        int xCapture = std::max( start.first, end.first ) - 1;
+        int yCapture = std::max( start.second, end.second ) - 1;
+        captureCell = &boardState_[ xCapture ][ yCapture ];
+        switch ( *captureCell ) {
+            case CellState::EMPTY: return false;
+            case CellState::RED: [[fallthrough]];
+            case CellState::RED_KING: if ( redTurn ) return false;
+                break;
+            case CellState::BLACK: [[fallthrough]];
+            case CellState::BLACK_KING: if ( !redTurn ) return false;
+                break;
+        }
+    }
+
+    // change state of cells and capture piece if applicable
+    endCell   = startCell;
+    startCell = CellState::EMPTY;
+    if ( captureCell ) *captureCell = CellState::EMPTY;
+
+    // promote piece to king if applicable
+    if ( endCell == CellState::BLACK && end.second == 7 ) endCell = CellState::BLACK_KING;
+    else if ( endCell == CellState::RED && end.second == 0 ) endCell = CellState::RED_KING;
+
+    redTurn = !redTurn;
+
     return true;
 }
